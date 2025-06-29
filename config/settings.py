587e3d8a85006f7b4d548de399b09410d770a967
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-06-29 01:57:51 -0300

    chore: configuracao CORS

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'professionals',
     
@@ -51,6 +52,7 @@ REST_FRAMEWORK = {
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,6 +60,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",  # Vite
 ]
 
 ROOT_URLCONF = 'config.urls'
--- professionals/urls.py
+++ professionals/urls.py
@@ -1,13 +1,11 @@
-from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from professionals.views import ProfessionalViewSet, ConsultationViewSet
+from rest_framework.routers import DefaultRouter
+from .views import ProfessionalViewSet, ConsultationViewSet
 
-router = routers.DefaultRouter()
+router = DefaultRouter()
 router.register(r'professionals', ProfessionalViewSet)
 router.register(r'consultations', ConsultationViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
 ]
